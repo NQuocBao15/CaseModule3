8582148685d023b720cc7079b800043b80f4e754
DucTrann1310 <prductran1310@gmail.com>
2023-10-02 11:17:36 +0700

    lam lai

---
+++ Case3/src/main/java/Model/Product.java
@@ -0,0 +1,72 @@
+package Model;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private int id;
+    private String name;
+    private BigDecimal price;
+    private String img;
+    private String description;
+    private Category category;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, BigDecimal price, String img, String description, Category category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.img = img;
+        this.description = description;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
